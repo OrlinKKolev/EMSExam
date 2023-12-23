1c813823da04e1eb0be244012733d05e8bb99ca5
OrlinKolev <orlin.k.kolev@gmail.com>
2023-12-23 12:16:48 +0200

    Initial commit - project setup.

---
+++ src/main/java/com/academy/EMSExam/EmsExamApplication.java
@@ -0,0 +1,13 @@
+package com.academy.EMSExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/EMSExam/EmsExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.EMSExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
