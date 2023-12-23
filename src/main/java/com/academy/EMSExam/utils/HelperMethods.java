266e515475e644e6d1a2f2f9717d5f34e2c321fe
OrlinKolev <orlin.k.kolev@gmail.com>
2023-12-23 16:39:03 +0200

    null DateTo handler revised

--- src/main/java/com/academy/EMSExam/utils/HelperMethods.java
+++ src/main/java/com/academy/EMSExam/utils/HelperMethods.java
@@ -1,5 +1,8 @@
 package com.academy.EMSExam.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelperMethods {
 
 	public static boolean hasHeaders = false;
@@ -15,6 +18,26 @@ public class HelperMethods {
 			return false;
 		}
 		return true;
+	}
+
+
+
+	public static String nullStringConverter(String text) {
+//		List<String> nulls = new ArrayList<>();
+//		nulls.add(" ");
+//		nulls.add("null");
+//		nulls.add("Null");
+//		nulls.add("nUll");
+//		nulls.add("nuLl");
+//		nulls.add("nulL");
+//text.
+//		nulls.add("NULL");
+
+		if (text.equalsIgnoreCase("null") || text.equals(" ")) {
+			return null;
+		} else {
+			return text;
+		}
 	}
 
 }
