f2f76190790d63db75377a445bc7e1578f1cb4e8
OrlinKolev <orlin.k.kolev@gmail.com>
2023-12-23 15:46:25 +0200

    null value in "DateTo" field condition added

--- src/main/java/com/academy/EMSExam/utils/CustomDateFormatter.java
+++ src/main/java/com/academy/EMSExam/utils/CustomDateFormatter.java
@@ -3,18 +3,23 @@ package com.academy.EMSExam.utils;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.util.Objects;
 
 public class CustomDateFormatter {
 	public static LocalDate parseDate(String date) {
-		DateTimeFormatterBuilder dateTimeFormatterBuilder = new DateTimeFormatterBuilder()
-				.append(DateTimeFormatter
-						.ofPattern("[yyyyMMdd]" +
-								"[yyyy-MM-dd]" +
-								"[yyyy.MM.dd]" +
-								"[MM/dd/yyyy]" +
-								"[dd-MM-yyyy]" +
-								"[dd.MM.yyyy]"));
-		DateTimeFormatter dateTimeFormatter = dateTimeFormatterBuilder.toFormatter();
-		return LocalDate.parse(date, dateTimeFormatter);
+		if (Objects.equals(date, "null")) {
+			return LocalDate.now();
+		} else {
+			DateTimeFormatterBuilder dateTimeFormatterBuilder = new DateTimeFormatterBuilder()
+					.append(DateTimeFormatter
+							.ofPattern("[yyyyMMdd]" +
+									"[yyyy-MM-dd]" +
+									"[yyyy.MM.dd]" +
+									"[MM/dd/yyyy]" +
+									"[dd-MM-yyyy]" +
+									"[dd.MM.yyyy]"));
+			DateTimeFormatter dateTimeFormatter = dateTimeFormatterBuilder.toFormatter();
+			return LocalDate.parse(date, dateTimeFormatter);
+		}
 	}
 }
