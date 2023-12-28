8b4aaff3c16f6dbfdc0746b07511493e9a6c8f84
OrlinKolev <orlin.k.kolev@gmail.com>
2023-12-28 11:29:19 +0200

    refactored with methods calls

--- src/main/java/com/academy/EMSExam/MainTest.java
+++ src/main/java/com/academy/EMSExam/MainTest.java
@@ -1,56 +1,32 @@
 package com.academy.EMSExam;
 
 import com.academy.EMSExam.dto.LineDTO;
-import com.academy.EMSExam.fileOperations.*;
+import com.academy.EMSExam.model.Pair;
+import com.academy.EMSExam.repository.CSVRepository;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.*;
 
 public class MainTest {
 	public static void main(String[] args) {
-		//reader part
-		CustomReader reader = new CSVReader();
-		var resources = reader.read("./src/main/resources/files/generated_data.csv");
+
+		var resources = CSVRepository.read("./src/main/resources/files/generated_data.csv");
+		var employees = listAllEmployees(resources);
+		var projects = listAllProjects(resources);
+		var projectsByParis = listProjectsByPairs(resources,projects);
+
 		System.out.println(resources);
-		List<Integer> employees = new ArrayList<>();
-		List<Integer> projects = new ArrayList<>();
-
-		//crate lists with unique values. I use List instead of Set, because I want to get values by index
-		for (Serializable resource : resources) {
-			LineDTO dto = (LineDTO) resource;
-			if (!employees.contains(dto.getEmpId())) {
-				employees.add(dto.getEmpId());
-			}
-			if (!projects.contains(dto.getProjectId())) {
-				projects.add(dto.getProjectId());
-			}
-		}
-		//List with all employees in the records
 		System.out.println(employees);
-		//List with all projects in the records
 		System.out.println(projects);
-
-		//loops through all items and put in list with unique EmpIDs
-		HashMap<Integer, Set<Integer>> projectsWithPairs = new HashMap<>();
-		int projectIdMatcher = 1;
-		for (int i = 0; i < projects.size(); i++) {
-			Set<Integer> employeesPerProject = new HashSet<>();
-			//
-			for (Serializable resource : resources) {
-				LineDTO dto = (LineDTO) resource;
-				if (dto.getProjectId() == projectIdMatcher) {
-					employeesPerProject.add(dto.getEmpId());
-				}
-			}
-			System.out.printf("ProjID:%d, EmpID: %s %n",projectIdMatcher,employeesPerProject);
-			projectsWithPairs.put(projectIdMatcher, employeesPerProject);
-			projectIdMatcher++;
-		}
-		System.out.println(projectsWithPairs);
+		System.out.println(projectsByParis);
 
 
+		Pair pair1 = new Pair(5, 102, LocalDate.parse("2023-12-19"), LocalDate.parse("2023-12-27"), 103, LocalDate.parse("2023-12-18"), LocalDate.parse("2023-12-26"));
+		System.out.println(pair1.period());
+		System.out.println(pair1.getPairid());
 
-//		//writer part
+		//		//writer part
 //		List<Serializable> parsedData = new ArrayList<>();
 //		CustomWriter writer = new CSVWriter();
 //		parsedData.addAll(resources);
@@ -74,4 +50,49 @@ public class MainTest {
 
 	}
 
+	public static List<Integer> listAllEmployees(List<? extends Serializable> resources) {
+		//crate lists with unique values. Use List instead of Set, because I want to get values by index
+		List<Integer> employees = new ArrayList<>();
+		for (Serializable resource : resources) {
+			LineDTO dto = (LineDTO) resource;
+			if (!employees.contains(dto.getEmpId())) {
+				employees.add(dto.getEmpId());
+			}
+		}
+		//List with all employees in the records
+		return employees;
+	}
+
+	public static List<Integer> listAllProjects(List<? extends Serializable> resources) {
+		//crate lists with unique values. Use List instead of Set, because I want to get values by index
+		List<Integer> projects = new ArrayList<>();
+		for (Serializable resource : resources) {
+			LineDTO dto = (LineDTO) resource;
+			if (!projects.contains(dto.getProjectId())) {
+				projects.add(dto.getProjectId());
+			}
+		}
+		return projects;
+	}
+
+	public static HashMap<Integer, Set<Integer>> listProjectsByPairs(List<? extends Serializable> resources, List<? extends Serializable> projects) {
+		//loops through all items and put in list with unique EmpIDs
+		HashMap<Integer, Set<Integer>> projectsWithPairs = new HashMap<>();
+		int projectIdMatcher = 1;
+		for (
+				int i = 0; i < projects.size(); i++) {
+			Set<Integer> employeesPerProject = new HashSet<>();
+			//
+			for (Serializable resource : resources) {
+				LineDTO dto = (LineDTO) resource;
+				if (dto.getProjectId() == projectIdMatcher) {
+					employeesPerProject.add(dto.getEmpId());
+				}
+			}
+			System.out.printf("ProjID:%d, EmpID: %s %n", projectIdMatcher, employeesPerProject);
+			projectsWithPairs.put(projectIdMatcher, employeesPerProject);
+			projectIdMatcher++;
+		}
+		return projectsWithPairs;
+	}
 }
