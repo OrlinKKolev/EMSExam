f2a15ebb7d8caa7237cec0bf922932a5c825fb36
OrlinKolev <orlin.k.kolev@gmail.com>
2023-12-31 11:10:29 +0200

    local variables implemented in reader

--- src/main/java/com/academy/EMSExam/fileoperations/CSVReader.java
+++ src/main/java/com/academy/EMSExam/fileoperations/CSVReader.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,13 +32,13 @@ public class CSVReader implements CustomReader {
 						rowAdd = 2;
 					}
 					try {
-						employeeRecord.add(new LineDTO(Integer.parseInt(values[0]),
-								Integer.parseInt(values[1]),
-								CustomDateFormatter.parseDate((values[2])),
-								CustomDateFormatter.parseDate(
-										HelperMethods.nullStringConverter(values[3]))));
+						int empId = Integer.parseInt(values[0]);
+						int projectId = Integer.parseInt(values[1]);
+						LocalDate dateFrom = CustomDateFormatter.parseDate((values[2]));
+						LocalDate dateTo = CustomDateFormatter.parseDate(HelperMethods.nullStringConverter(values[3]));
+						employeeRecord.add(new LineDTO(empId, projectId, dateFrom, dateTo));
 					} catch (DateTimeParseException e) {
-						System.out.printf("Date format parse not possible row %d, starting with id %s%n", employeeRecord.size() + rowAdd, values[0]);
+						System.out.printf("Date format parse not possible on row %d, starting with id %s%n", employeeRecord.size() + rowAdd, values[0]);
 						System.out.println("Check if entered data is a valid date");
 						System.out.println("For supported date formats check in the readme file.");
 						break;
