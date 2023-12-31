b82e8d3ca8a057f5101dd2e94769661dcc6fda41
OrlinKolev <orlin.k.kolev@gmail.com>
2023-12-31 10:29:27 +0200

    removed unnecessary functionality

--- src/main/java/com/academy/EMSExam/fileoperations/CustomWriter.java
+++
@@ -1,8 +0,0 @@
-package com.academy.EMSExam.fileoperations;
-
-import java.io.Serializable;
-import java.util.List;
-
-public interface CustomWriter {
-	void write (List<? extends Serializable> items, String filename);
-}
